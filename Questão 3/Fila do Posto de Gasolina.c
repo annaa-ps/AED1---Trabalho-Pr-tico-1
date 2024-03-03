72adadf3620e4999e5fb9c7ca3cf0ba27c7ec801
Anna Karolyna <129415573+annaa-ps@users.noreply.github.com>
2024-03-03 16:40:31 -0300

    Update Fila do Posto de Gasolina.c

--- Questão 3/Fila do Posto de Gasolina.c
+++ Questão 3/Fila do Posto de Gasolina.c
@@ -1,201 +1,190 @@
-/*
-    O seguinte programa consiste em criar e testar as funcionalidades de uma fila de carros em um posto de gasolina.
-
-    Grupo:
-
-    **Nome 1: ** **Matricula: **
-
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// Struct dos carros, tamanho da placa é baseado no tamanho atual brasileiro, tamanho de modelo é um valor arbitrário para
-// que não gaste muita memoria atoa para a demonstracao.
+// Definição da estrutura de dados para os carros
 typedef struct {
     char placa[8];
     char modelo[11];
     int anoDeFabricacao;
 } Carro;
 
-// Struct da fila utilizada, maxtam define o tamanho maximo e tam define o tamanho atual.
+// Definição da estrutura de dados para a fila de carros
 typedef struct {
-    Carro* carros;
-    int tam;
-    int maxtam;
-    int fim;
-    int inicio;
+    Carro* carros; // Ponteiro para a array de carros
+    int tam; // Tamanho atual da fila
+    int maxtam; // Capacidade máxima da fila
+    int fim; // Índice do último elemento na fila
+    int inicio; // Índice do primeiro elemento na fila
 } Fila;
 
-// Verifica se a fila está vazia.
+// Função para verificar se a fila está vazia
 int filaVazia(Fila* f) {
     if (f == NULL) return -1;
     if (f->tam == 0) return 0;
     else return 1;
 }
 
-// Verifica se a fila esta cheia.
+// Função para verificar se a fila está cheia
 int filaCheia(Fila* f) {
     if (f->tam == f->maxtam) return 0;
     else return 1;
 }
 
-// Cria a fila nova com o tamanho passado pelo programa.
+// Função para criar uma fila com uma capacidade máxima especificada
 int criaFila(Fila* f, int maxtam) {
     f->maxtam = maxtam;
-    f->carros = (Carro*)malloc(maxtam * sizeof(Carro));
-    if (f->carros == NULL) return -1;
+    f->carros = (Carro*)malloc(maxtam * sizeof(Carro)); // Aloca memória para a array de carros
+    if (f->carros == NULL) return -1; // Retorna -1 em caso de falha na alocação de memória
     f->tam = 0;
     f->inicio = 0;
     f->fim = -1;
-    return 0;
+    return 0; // Retorna 0 em caso de sucesso na criação da fila
 }
 
-// Funcao que adiociona um carro na fila.
+// Função para adicionar um carro à fila
 int adicionarNaFila(Fila* f, Carro carro) {
     if (f == NULL) return -1;
-    if (filaCheia(f) == 0) return -2;
+    if (filaCheia(f) == 0) return -2; // Retorna -2 se a fila estiver cheia
 
-    f->fim = (f->fim + 1) % f->maxtam;
-    f->carros[f->fim] = carro;
-    f->tam++;
+    f->fim = (f->fim + 1) % f->maxtam; // Incrementa o índice do último elemento circularmente
+    f->carros[f->fim] = carro; // Adiciona o carro ao final da fila
+    f->tam++; // Incrementa o tamanho da fila
 
-    return 0;
+    return 0; // Retorna 0 em caso de sucesso na adição do carro à fila
 }
 
-// Essa funcao remove da fila, ela é a função que determina o atendimento de um cliente, retorna o carro atendido para uso em outras funcoes.
+// Função para remover um carro da fila
 int removerDaFila(Fila* f, Carro* carroAtendido) {
     if (f == NULL) return -1;
-    if (filaVazia(f) == 0) return -3;
+    if (filaVazia(f) == 0) return -3; // Retorna -3 se a fila estiver vazia
 
-    *carroAtendido = f->carros[f->inicio];
-    f->inicio = (f->inicio + 1) % f->maxtam;
-    f->tam--;
+    *carroAtendido = f->carros[f->inicio]; // Carro no início da fila é removido
+    f->inicio = (f->inicio + 1) % f->maxtam; // Incrementa o índice do primeiro elemento circularmente
+    f->tam--; // Decrementa o tamanho da fila
 
-    if(f->tam == 0){
-      f->inicio = -1;
+    if (f->tam == 0) {
+        f->inicio = -1; // Se a fila estiver vazia, define o índice do primeiro elemento como -1
     }
-    return 0;
+
+    return 0; // Retorna 0 em caso de sucesso na remoção do carro da fila
 }
 
-// Procura um carro na fila, essa funcao foi propositalmente feita quebrando as regras de fila e acessando todos os elementos para demonstracao dessa possibilidade.
+// Função para buscar um carro na fila pelo número da placa
 int buscarPlaca(Fila* f, char* placa, Carro* carroEncontrado) {
     if (f == NULL) return -1;
-    if (filaVazia(f) == 0) return -3;
+    if (filaVazia(f) == 0) return -3; // Retorna -3 se a fila estiver vazia
 
-    for (int i = 0; i < f->tam; i++)
+    for (int i = 0; i < f->tam; i++) {
         if (strcmp(f->carros[(f->inicio + i) % f->maxtam].placa, placa) == 0) {
-            *carroEncontrado = f->carros[(f->inicio + i) % f->maxtam];
-            return 5;
+            *carroEncontrado = f->carros[(f->inicio + i) % f->maxtam]; // Encontrou o carro com a placa especificada
+            return 5; // Retorna 5 para indicar que o carro foi encontrado na fila
         }
+    }
 
-    return 0;
+    return 0; // Retorna 0 se o carro não for encontrado na fila
 }
 
-// Essa funcao mostra a fila inteira, foi propositalmente feita MANTENDO as regras de fila, que eh a regra de que apenas o primeiro elemento esta disponivel
-// dessa forma ela retira o item e o coloca no final da fila novamente, dessa forma ela percorre a fila inteira sem infringir as regras.
-int mostrarFila (Fila *f){
+// Função para exibir todos os carros na fila
+int mostrarFila(Fila *f) {
     if (f == NULL) return -1;
-    if (filaVazia(f) == 0) return -3;
+    if (filaVazia(f) == 0) return -3; // Retorna -3 se a fila estiver vazia
 
     Carro carroParaAtender;
-    for(int i = 0; i < f->tam; i++){
-        removerDaFila(f, &carroParaAtender);
+    for (int i = 0; i < f->tam; i++) {
+        removerDaFila(f, &carroParaAtender); // Remove o carro da fila e o exibe
         printf("\nPlaca: %s, Modelo: %s, Ano de Fabricacao: %d\n", carroParaAtender.placa, carroParaAtender.modelo, carroParaAtender.anoDeFabricacao);
-        adicionarNaFila(f, carroParaAtender);
+        adicionarNaFila(f, carroParaAtender); // Adiciona o carro de volta à fila
     }
-    return 0;
+    return 0; // Retorna 0 em caso de sucesso ao exibir a fila
 }
 
-// Funcao para aumentar o tamanho da fila caso necessário.
-int aumentarFila(Fila* f, int aumentarEm) {
-    if (f == NULL) return -1;
-
-    Carro* aumentado = realloc(f->carros, (f->maxtam + aumentarEm) * sizeof(Carro));
-    if (aumentado == NULL) return -4;
-    f->carros = aumentado;
-    f->maxtam += aumentarEm;
-
-    return 0;
-}
-
+// Função para exibir o carro mais recentemente atendido
 void exibirCarroAtendidoRecente(Carro* carro) {
-    printf("Carro atendido recentemente:\n");
+    printf("\nCarro atendido recentemente:\n");
     printf("Placa: %s, Modelo: %s, Ano de Fabricacao: %d\n", carro->placa, carro->modelo, carro->anoDeFabricacao);
 }
 
-int mostrarVeiculosAtendidos(Fila *f){
-    if(f == NULL) return -1;
-    if(filaVazia(f) == 0) return -3;
+// Função para exibir todos os carros atendidos
+void mostrarVeiculosAtendidos(Fila *atendidos) {
+    if (atendidos == NULL) {
+        printf("\nNenhum carro foi atendido ainda.\n"); // Se a fila de carros atendidos estiver vazia
+        return;
+    }
 
     printf("\nCarros atendidos:\n");
-    exibirCarroAtendidoRecente(&f->carros[(f->inicio) % f->maxtam]);
-    return 0;
+    for (int i = 0; i < atendidos->tam; i++) {
+        printf("Placa: %s, Modelo: %s, Ano de Fabricacao: %d\n", atendidos->carros[i].placa, atendidos->carros[i].modelo, atendidos->carros[i].anoDeFabricacao);
+    }
 }
 
+// Função principal
 int main() {
-    Fila fila;
-    if (criaFila(&fila, 5) != 0) {
-        printf("Erro ao criar a fila.\n");
-        return 1;
-    }
+    Fila fila, atendidos;
+    criaFila(&fila, 5); // Cria a fila de carros
+    criaFila(&atendidos, 5); // Cria a fila de carros atendidos
 
     int opcao;
     char placa[8];
     Carro carro, carroEncontrado, carroAtendido; 
 
-    do{
-      printf("\nMENU:\n");
-      printf("1 - Adicionar carro na fila\n");
-      printf("2 - Atender um carro\n");
-      printf("3 - Verificar se um carro esta na fila\n");
-      printf("4 - Exibir carros esperando atendimento\n");
-      printf("5 - Exibir carros que ja foram atendidos\n");
-      printf("6 - Sair\n");
-      printf("Informe a opcao desejada:\n");
-      scanf("%d", &opcao);
+    do {
+        printf("\nMENU:\n");
+        printf("1 - Adicionar carro na fila\n");
+        printf("2 - Atender um carro\n");
+        printf("3 - Verificar se um carro esta na fila\n");
+        printf("4 - Exibir carros esperando atendimento\n");
+        printf("5 - Exibir carros que ja foram atendidos\n");
+        printf("6 - Sair\n");
+        printf("Informe a opcao desejada:\n");
+        scanf("%d", &opcao);
 
-      switch(opcao){
-        case 1:
-          printf("\nInforme a placa do carro:\n");
-          scanf("%s", carro.placa);
-          printf("Informe o modelo do carro:\n");
-          scanf("%s", carro.modelo);
-          printf("Informe o ano de fabricacao do carro:\n");
-          scanf("%d", &carro.anoDeFabricacao);
-          adicionarNaFila(&fila, carro);
-          break;
-        case 2:
-          if(removerDaFila(&fila, &carroAtendido) == 0){
-            printf("\nCarro atendido: Placa: %s, Modelo: %s, Ano de Fabricacao: %d\n", carroAtendido.placa, carroAtendido.modelo, carroAtendido.anoDeFabricacao); 
-          }else{
-            printf("\nNao ha carros na fila!\n");
-          }
-          break;
-        case 3: 
-          printf("\nInforme a placa do carro:\n");
-          scanf("%s", placa);
-          if(buscarPlaca(&fila, placa, &carroEncontrado)){
-            printf("\nO carro com a placa %s esta na fila\n", placa);
-          }else{
-            printf("\nO carro com a placa %s nao esta na fila\n", placa);
-          }
-          break;
-        case 4: 
-          printf("\nCarros esperando atendimento:\n");
-          mostrarFila(&fila);
-          break;;
-        case 5: 
-          mostrarVeiculosAtendidos(&fila);
-          break;
-        case 6:
-          printf("\nSaindo...\n");
-          break;
-        default:
-          printf("\nOpcao invalida!\n");
-          break;
-      }
-    }while(opcao != 6);
+        switch(opcao) {
+            case 1:
+                printf("\nInforme a placa do carro:\n");
+                scanf("%s", carro.placa);
+                printf("Informe o modelo do carro:\n");
+                scanf("%s", carro.modelo);
+                printf("Informe o ano de fabricacao do carro:\n");
+                scanf("%d", &carro.anoDeFabricacao);
+                adicionarNaFila(&fila, carro);
+                break;
+            case 2:
+                if (removerDaFila(&fila, &carroAtendido) == 0) {
+                    exibirCarroAtendidoRecente(&carroAtendido); 
+                    adicionarNaFila(&atendidos, carroAtendido); // Adiciona o carro atendido à fila de carros atendidos
+                } else {
+                    printf("\nNao ha carros na fila!\n");
+                }
+                break;
+            case 3: 
+                printf("\nInforme a placa do carro:\n");
+                scanf("%s", placa);
+                if (buscarPlaca(&fila, placa, &carroEncontrado)) {
+                    printf("\nO carro com a placa %s esta na fila\n", placa);
+                } else {
+                    printf("\nO carro com a placa %s nao esta na fila\n", placa);
+                }
+                break;
+            case 4: 
+                printf("\nCarros esperando atendimento:\n");
+                mostrarFila(&fila);
+                break;
+            case 5: 
+                mostrarVeiculosAtendidos(&atendidos);
+                break;
+            case 6:
+                printf("\nSaindo...\n");
+                break;
+            default:
+                printf("\nOpcao invalida!\n");
+                break;
+        }
+    } while (opcao != 6);
+
+    // Libera a memória alocada para as filas
+    free(fila.carros);
+    free(atendidos.carros);
 
     return 0;
 }
